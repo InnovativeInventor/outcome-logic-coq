305623a65aa9c4b582019407cf475802158627ee
Emmanuel Suarez Acevedo <19786004+emmanueljs1@users.noreply.github.com>
2024-11-19 20:51:53 -0500

    cleanup

--- theories/syntax.v
+++ theories/syntax.v
@@ -37,17 +37,20 @@ Import MonadNotation.
 Local Open Scope monad_scope.
 Local Open Scope string_scope.
 
+(* expressions *)
 Inductive expr : Type :=
 | Var : nat -> expr
 | True : expr
 | False : expr
 .
 
+(* atomic commands *)
 Inductive cmd : Type :=
 | assume : expr -> cmd
 | assign : nat -> expr -> cmd
 .
 
+(* command language *)
 Inductive cl : Type :=
 | Zero
 | One
@@ -63,85 +66,87 @@ Notation "C ⋆" := (Star C) (at level 60).
 Notation "C1 + C2" := (Branch C1 C2).
 Notation "C1 ⨟ C2" := (Seq C1 C2) (at level 80).
 
-Inductive weight : Type :=
-| Bool : bool -> weight
-| Unit : weight
+Inductive value : Type :=
+| Bool : bool -> value
+| Unit : value
 .
 
-Inductive noop : Type -> Type :=
-| tt : noop unit.
+Definition state := nat -> value.
 
-(* monad (comes with >>= and ret) *)
-Notation M := (ctree noop B02).
+Parameter effect : Type -> Type.
 
-(* state *)
-Definition Σ := nat -> weight.
+Notation computation := (ctree effect B02).
 
-Definition insert (x : nat) (w : weight) (st : Σ) : Σ :=
-  fun y => if Nat.eq_dec x y then w else st y.
+(* computation is a monad (>>= and ret already defined)
+ * and a monoid *)
 
-Definition denote_expr (e : expr) (st: Σ) : weight :=
+(* monoid identity *)
+Notation "∅" := (Stuck : computation state).
+(* monoid addition *)
+Notation "x ◇ y" := (br2 x y) (at level 60).
+
+Definition insert (x : nat) (w : value) (σ : state) : state :=
+  fun y => if Nat.eq_dec x y then w else σ y.
+
+Definition denote_expr (e : expr) (σ : state) : value :=
   match e with
-  | Var x => st x
+  | Var x => σ x
   | True => Bool true
   | False => Bool false
   end.
 
-Definition weight_to_bool (w : weight) : bool :=
-  match w with
+Definition value_to_bool (v : value) : bool :=
+  match v with
   | Bool b => b
   | Unit => false
   end.
 
-(* monoid identity *)
-Notation "∅" := (Stuck : M Σ).
-(* monoid addition *)
-Notation "x ◇ y" := (br2 x y) (at level 60).
-
-Definition denote_cmd (c : cmd) (st: Σ) : M Σ :=
+Definition denote_cmd (c : cmd) (σ : state) : computation state :=
   match c with
   | assume e =>
-      if weight_to_bool (denote_expr e st) then ret st else ∅
-  | assign x e => ret (insert x (denote_expr e st) st)
+      if value_to_bool (denote_expr e σ) then ret σ else ∅
+  | assign x e =>
+      ret (insert x (denote_expr e σ) σ)
   end.
 
 Reserved Notation "⟦ c ⟧".
 
-Fixpoint denote (C : cl) (st : Σ) : M Σ :=
+Fixpoint denote (C : cl) (σ : state) : computation state :=
   match C with
   | 𝟘 => ∅
-  | 𝟙 => ret st
-  | C1 ⨟ C2 => ⟦ C1 ⟧ st >>= ⟦ C2 ⟧
-  | C1 + C2 => (⟦ C1 ⟧ st) ◇ (⟦ C2 ⟧ st)
+  | 𝟙 => ret σ
+  | C1 ⨟ C2 => ⟦ C1 ⟧ σ >>= ⟦ C2 ⟧
+  | C1 + C2 => ⟦ C1 ⟧ σ ◇ ⟦ C2 ⟧ σ
   | C ⋆ =>
-      iter (fun st' => (st'' <-  ⟦ C ⟧ st' ;; ret (inl st''))
-                         ◇
-                       (ret (inr st'))) st
-  | Atom cmd => denote_cmd cmd st
+      iter (fun σ' =>
+              (σ'' <-  ⟦ C ⟧ σ' ;; ret (inl σ''))
+                ◇
+                ret (inr σ')) σ
+  | Atom cmd => denote_cmd cmd σ
   end
 where "⟦ C ⟧" := (denote C).
 
-Theorem monoid_identity_l (m : M Σ) :
+Theorem monoid_identity_l (m : computation state) :
   ∅ ◇ m ~ m.
 Proof.
   apply br2_stuck_l.
 Qed.
 
-Theorem monoid_identity_r (m : M Σ) :
+Theorem monoid_identity_r (m : computation state) :
   m ◇ ∅ ~ m.
 Proof.
   apply br2_stuck_r.
 Qed.
 
-Theorem monoid_commutative (m1 m2 : M Σ) :
+Theorem monoid_commutative (m1 m2 : computation state) :
   m1 ◇ m2 ~ m2 ◇ m1.
 Proof.
   intros.
   apply br2_commut.
 Qed.
 
-Theorem monoid_addition_preserves_bind (m1 m2 : M Σ)
-  (k : Σ -> M Σ) :
+Theorem monoid_addition_preserves_bind (m1 m2 : computation state)
+  (k : state -> computation state) :
   (m1 ◇ m2) >>= k ~ (m1 >>= k) ◇ (m2 >>= k).
 Proof.
   intros. apply equ_sbisim_subrelation.
@@ -150,9 +155,7 @@ Proof.
     apply br_equ. intros. destruct t; reflexivity.
 Qed.
 
-Print sb.
-
-Theorem monoid_identity_cancels_bind (k : Σ -> M Σ) :
+Theorem monoid_identity_cancels_bind (k : state -> computation state) :
   ∅ >>= k ~ ∅.
 Proof.
   intros. apply equ_sbisim_subrelation.
