907ae95a7a05b3dd3d9c15563815b3e5a4deec2e
Emmanuel Suarez Acevedo <19786004+emmanueljs1@users.noreply.github.com>
2024-12-09 14:25:00 -0500

    add soundness admitted theorem

--- theories/syntax.v
+++ theories/syntax.v
@@ -215,6 +215,12 @@ Fixpoint sat (m : computation state) (phi : assertion) : Prop :=
   end
 where "m ⊨ phi" := (sat m phi).
 
+Lemma sat_respects_sb m m' phi :
+  m ~ m' ->
+  m ⊨ phi ->
+  m' ⊨ phi.
+Proof. Admitted.
+
 Definition triple (phi : assertion) (C : cl) (psi : assertion) : Prop :=
   forall (m : computation state), m ⊨ phi -> (m >>= ⟦ C ⟧) ⊨ psi.
 
@@ -234,3 +240,34 @@ Example ex : ⊨ ⟨ ⊤ ⟩ assume Tru ⟨ ⊤ ⟩.
 Proof.
   intros ??. constructor.
 Qed.
+
+Reserved Notation "⊢ ⟨ phi ⟩ C ⟨ psi ⟩".
+
+Inductive rules : assertion -> cl -> assertion -> Prop :=
+| RuleZero : forall phi, rules phi 𝟘 ⊤⊕
+| RuleOne : forall phi, rules phi 𝟙 phi
+where "⊢ ⟨ phi ⟩ C ⟨ psi ⟩" := (rules phi C psi).
+
+Lemma rule_zero_sound phi :
+  ⊨ ⟨ phi ⟩ 𝟘 ⟨ ⊤⊕ ⟩.
+Proof.
+Admitted.
+
+Lemma rule_one_sound phi :
+  ⊨ ⟨ phi ⟩ 𝟙 ⟨ phi ⟩.
+Proof.
+  intros ??. simpl.
+  eapply sat_respects_sb.
+  - admit.
+  - eassumption.
+Admitted.
+
+Theorem rules_sound phi C psi :
+  ⊢ ⟨ phi ⟩ C ⟨ psi ⟩ ->
+  ⊨ ⟨ phi ⟩ C ⟨ psi ⟩.
+Proof.
+  intros. induction H.
+  - apply rule_zero_sound.
+  - apply rule_one_sound.
+Admitted.
+  
