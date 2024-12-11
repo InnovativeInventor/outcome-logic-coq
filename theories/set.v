7c65cc79bbb74241c8a891c984d91edfaa598138
Emmanuel Suarez Acevedo <19786004+emmanueljs1@users.noreply.github.com>
2024-12-11 13:43:04 -0500

    automation

--- theories/set.v
+++ theories/set.v
@@ -1,3 +1,5 @@
+Require Import util.
+
 Definition set (X : Type) := X -> Prop.
 
 Definition eq_set `{X : Type} (s1 : set X) (s2 : set X) : Prop :=
@@ -27,56 +29,25 @@ Definition bind `{X : Type} (s : set X) (f : X -> set X) : set X :=
 
 Notation "s >>= f" := (bind s f) (at level 70).
 
+Ltac solve_eq_set :=
+  repeat (progress (unfold ret, "∅", "∈", "◇", bind, "≡" in *; simpgoal)).
+
 Lemma bind_ret_r `{X : Type} (s : set X) : (s >>= ret) ≡ s.
-Proof.
-  intros x. split; intros H.
-  - destruct H as [? [? ?]]. unfold ret, "∈" in *. subst. assumption.
-  - eexists. split; eauto. reflexivity.
-Qed.
+Proof. intros ?. split; intros ?; solve_eq_set. Qed.
 
 Lemma bind_ret_l `{X : Type} (x : X) f : (ret x >>= f) ≡ f x.
-Proof.
-  intros ?. split; intros H.
-  - destruct H as [? [? ?]]. unfold ret, "∈" in *. subst. assumption.
-  - eexists. split; eauto. reflexivity.
-Qed.
+Proof. intros ?. split; intros ?; solve_eq_set. Qed.
     
 Lemma bind_assoc `{X : Type} (s : set X) f g :
   (s >>= f) >>= g ≡ s >>= (fun x => f x >>= g).
-Proof.
-  intros ?. split; intros H.
-  - repeat destruct H as [? [? ?]].
-    repeat (eexists; split; eauto).
-  - repeat destruct H as [? [? H]].
-    repeat (eexists; split; eauto).
-Qed.
+Proof. intros ?. split; intros ?; solve_eq_set. Qed.
 
 Lemma union_comm `{X : Type} (s1 s2 : set X) : s1 ◇ s2 ≡ s2 ◇ s1.
-Proof.
-  intros ?. split; intros.
-  - destruct H.
-    + right. assumption.
-    + left. assumption.
-  - destruct H.
-    + right. assumption.
-    + left. assumption.
-Qed.
+Proof. intros ?. split; intros ?; solve_eq_set. Qed.
 
 Lemma union_preserves_bind `{X : Type} (s1 s2 : set X) k :
   (s1 ◇ s2) >>= k ≡ (s1 >>= k) ◇ (s2 >>= k).
-Proof.
-  intros ?. split; intros H.
-  - destruct H as [? [? ?]]. destruct H.
-    + left. eexists. split; eassumption.
-    + right. eexists. split; eassumption.
-  - destruct H; destruct H as [? [? ?]].
-    + eexists. split. left. all: eassumption.
-    + eexists. split. right. all: eassumption.
-Qed.
+Proof. intros ?. split; intros ?; solve_eq_set. Qed.
 
 Lemma empty_cancels_bind `{X : Type} (k : X -> set X) : ∅ >>= k ≡ ∅.
-Proof.
-  intros ?. split; intros H.
-  1: destruct H as [? [? ?]].
-  all: inversion H.
-Qed.
+Proof. intros ?. split; intros ?; solve_eq_set. Qed.
---
+++ theories/util.v
@@ -0,0 +1,22 @@
+Ltac simpgoal :=
+  repeat (simpl in * ;
+          match goal with
+          | [ p : (_ * _)%type |- _ ] => destruct p
+          | [ H : _ /\ _ |- _ ] => destruct H
+          | [ H : _ <-> _ |- _ ] => destruct H
+          | [ H : exists _, _ |- _ ] => destruct H
+          | [ |- _ /\ _ ] => split
+          | [ H : (_,_) = (_,_) |- _ ] =>
+              injection H ; clear H ; intros ; try subst
+          | [ H : Some _ = Some _ |- _ ] =>
+              injection H ; clear H ; intros ; try subst
+          | [ H : Some _ = None |- _ ] => congruence
+          | [ H : None = Some _ |- _ ] => congruence
+          | [ H : ?x <> ?x |- _ ] => congruence
+          | [ H : ~ True |- _ ] => contradiction H ; auto
+          | [ H : False |- _ ] => contradiction H ; auto
+          | [ |- forall _, _ ] => intros
+          | [ H : ?x = ?y |- _ ] => subst
+          | [ H : _ \/ _ |- _ ] => destruct H
+          | _ => eauto
+  end).
