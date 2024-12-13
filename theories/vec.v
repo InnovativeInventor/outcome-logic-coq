02db8e4c84b104e3602364df205a648a065117c2
Emmanuel Suarez Acevedo <19786004+emmanueljs1@users.noreply.github.com>
2024-12-12 21:30:27 -0500

    add update function to vec

--- theories/vec.v
+++ theories/vec.v
@@ -10,23 +10,42 @@ Fixpoint vec (n : nat) (A : Type) : Type :=
 Definition append `{A : Type} `{n : nat} (l : vec n A) (a : A) : vec (S n) A :=
   (a , l).
 
-Fixpoint lookup `{A : Type} `{n : nat} (l : vec n A) (m : nat) : option A :=
+Fixpoint lookup `{A : Type} `{n : nat} (l : vec n A) (i : nat) : option A :=
   match n return (vec n A -> option A) with
   | O => fun _ => None
   | S n => fun '(a , l) =>
-             match m with
+             match i with
              | O => Some a
-             | S m => lookup l m
+             | S i => lookup l i
              end
   end l.
 
-Theorem lookup_le `{A : Type} `{n : nat} (l : vec n A) (m : nat) :
-  m < n ->
-  exists a, lookup l m = Some a.
+Fixpoint update `{A : Type} `{n : nat} (l : vec n A) (i : nat) (a : A) : vec n A :=
+  match n return (vec n A -> vec n A) with
+  | O => fun l => l
+  | S n => fun '(a' , l') =>
+             match i with
+             | O => (a , l')
+             | S i => (a' , update l' i a)
+             end
+  end l.
+    
+Theorem lookup_le `{A : Type} `{n : nat} (l : vec n A) (i : nat) :
+  i < n ->
+  exists a, lookup l i = Some a.
 Proof.
-  revert m l. induction n.
+  revert i l. induction n.
   - intros ?? Hc. inversion Hc.
-  - intros ??. induction m; intros Hle.
-    + destruct l; simpgoal.
-    + destruct l as [a' l]. assert (m < n) by lia; simpgoal.
+  - intros ??. induction i; intros; simpgoal.
+    assert (i < n) by lia. simpgoal.
+Qed.
+
+Theorem update_le `{A : Type} `{n : nat} (l : vec n A) (i : nat) (a : A) :
+  i < n ->
+  lookup (update l i a) i = Some a.
+Proof.
+  revert i l a. induction n.
+  - intros ??? Hc. inversion Hc.
+  - intros ???. induction i; intros; simpgoal.
+    assert (i < n) by lia. simpgoal.
 Qed.
